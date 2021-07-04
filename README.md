e93aa43a2f0920ba8f2386ce9a6ae509aa60b1ee
Clark <81452611+freezenleo@users.noreply.github.com>
2021-07-04 13:57:24 -0500

    set up questions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# readme-questions-prompt
+# Professional README Generator Starter Code
+
+[How to create a Professional README](./readme-guide.md)
---
+++ index.js
@@ -0,0 +1,75 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = ['What is your GitHub username?',
+    'What is your email address?',
+    'What is your project’s name?',
+    'Please write a short description of your project',
+    'What kind of license should your project have?',
+    'What command should be run to install dependencies?',
+    'What command should be run to run tests?',
+    'What does the user need to know about using the repo?',
+    'What does the user need to know about contributing to the repo?'
+];
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: questions[0]
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: questions[3]
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses',
+            message: questions[4],
+            choices: ['Apache', 'MIT', 'Perl', 'SIL', '']
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: questions[7]
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: questions[8]
+        },
+    ])
+}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
