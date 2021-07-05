9027a6490a8476084b68b75e8b927b7dcf63c5e2
Clark <81452611+freezenleo@users.noreply.github.com>
2021-07-05 17:26:03 -0500

    save a copy

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  return `
-  ![License](https://img.shields.io/badge/License-${answers.license}-blue.svg)
-  `
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  return `
-  https://opensource.org/licenses/${answers.license}
-  `
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  if (license === 'N/A') {
-    return '';
-  }
-
-}
+const licenseArr = {
+  'Apache 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+  'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+  'MPL 2.0': '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+  'Artistic 2.0': '[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)',
+  'Unlicense': '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let licenseSection = licenseArr[data.license];
+
   return `# ${data.title}
-  ${renderLicenseSection}
+  ${licenseSection}
   
   ## Description
   ${answers.description}
